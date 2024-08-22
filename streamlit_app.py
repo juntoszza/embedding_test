24e3a894f0a537ccc10b4f4ed6d489fd8b85d9d3
juntoszza <67109625+juntoszza@users.noreply.github.com>
2024-08-22 21:07:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import requests
 
+# Vertex AI 설정
+vertex_ai_endpoint = "https://<vertex-ai-endpoint>"
+embedding_model_endpoint = "https://asia-northeast3-aiplatform.googleapis.com/v1/projects/keyword-data-373606/locations/asia-northeast3/publishers/google/models/text-multilingual-embedding-002"
+access_token = "ya29.a0AcM612w7j2vM8hgttkC9sSEl8sKTBaL-nFrqMTY3-V1zIdXwjwwYKNrYtDkwAl6BfgR80HbdE2SXcuX9g1g822lE-nF2cviTJLhr8Xa6YrFKscnM3u8vJsMSC1KDo-zlePmJEnTqiMPQMQx1fu2Btsl-Gd3MDUYPZtw7PORS9PcbsQaCgYKAXsSARESFQHGX2MiacXHD4Ns2vIB2oWGheil-g0181"
+index_endpoint_name = "embedding_test"
+deployed_index_id = "vertex_embedding_test_1724296756679"
+
+
 
 def get_embedding_from_model(query):
     headers = {
